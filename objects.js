510a31a1602b9807ea7e556cadc35da78bac9e37
morgane@restlessthinking.com <morgane@restlessthinking.com>
2018-06-20 04:21:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,4 +2,12 @@ var playlist = {
   "Maccabees": "Toothpaste Kisses"
 }
 
-function
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removePlaylist (playlist, artistName, songTitle) {
+  delete playlist.artistName
+  return playlist
+}
